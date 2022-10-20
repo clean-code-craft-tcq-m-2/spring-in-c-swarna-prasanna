75e9afff1e69db7544ddd07213db3a7938a7748a
swarna-prasanna <51480404+swarna-prasanna@users.noreply.github.com>
2022-10-20 14:57:44 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -6,22 +6,29 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
 	s.min = 0;
 	s.max = 0;
 	float average = 0;
-	
-	average = s.min = s.max = *numberset;
-
-	for(int i=1; i<=setlength-1; i++)
+	if(setlength > 0)
 	{
-		average = average + numberset[i];
-		if(s.min > numberset[i])
-			s.min = numberset[i];
-		if(s.max < numberset[i])
-			s.max = numberset[i];
-	}
-	s.average = average / setlength;
-	printf("minimum value in numberset array is : %d",s.min);
-	printf("maximum value in numberset array is : %d",s.max);
-	printf("average value in numberset array is : %d",s.average);
+		average = s.min = s.max = *numberset;
 
+		for(int i=1; i<=setlength-1; i++)
+		{
+			average = average + numberset[i];
+			if(s.min > numberset[i])
+				s.min = numberset[i];
+			if(s.max < numberset[i])
+				s.max = numberset[i];
+		}
+		s.average = average / setlength;
+		printf("minimum value in numberset array is : %d",s.min);
+		printf("maximum value in numberset array is : %d",s.max);
+		printf("average value in numberset array is : %d",s.average);
+	}
+	else
+	{
+	    s.average = NAN;
+		s.min = NAN;
+		s.max = NAN;	
+	}
 	return s;
 
 }
